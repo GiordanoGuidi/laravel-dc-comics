a7fd18a001100db3c2c3fea6c7755e30b5e1334c
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-03-13 19:04:56 +0100

    modified structure file home.blade.php

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -6,7 +6,13 @@ use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
+    //Funzione per mostare la lista dei fumetti
     public function index()
+    {
+    }
+
+    //Funzione per mostare il singolo fumetto
+    public function show()
     {
     }
 }
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,3 +1,30 @@
-<div>
-    <!-- Order your soul. Reduce your wants. - Augustine -->
-</div>
+@extends('layouts.main')
+
+@section('title', 'Comics')
+
+
+@section('main-content')
+{{--Main--}}
+    <!--CARDS-CONTAINER-->
+    <div class="comics-container">
+        <section id="comics-list" class="small-container card-container">
+            <button class="series">CURRENT SERIES</button>
+            <button class="load">LOAD MORE</button>
+            <ul class="cards-list">
+                @foreach ( $comics as $index => $comic )
+                <!--SINGLE-CARD-->
+                <li class="comic-book-card">
+                    <a href="{{route('products.show',$index)}}">
+                        <div class="thumb">
+                            <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
+                        </div>
+                        <h3>{{ $comic['series'] }}</h3>
+                    </a>
+                </li>
+                @endforeach
+            </ul>
+        </section>
+    </div>
+    @include('includes.dc_shop')
+@endsection
+</html>
