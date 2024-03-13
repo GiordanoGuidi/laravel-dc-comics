d82e40008bcce87303d4fe941a1a4e00fa70ff8f
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-03-13 16:18:28 +0100

    create database comics_db

--- database/migrations/2024_03_13_144815_create_comics_table.php
+++ database/migrations/2024_03_13_144815_create_comics_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 150)->unique();
+            $table->text('description')->nullable();
+            $table->string('thumb');
+            $table->string('price', 10);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type', 50);
+            $table->string('artists', 150);
+            $table->string('writers', 150);
             $table->timestamps();
         });
     }
